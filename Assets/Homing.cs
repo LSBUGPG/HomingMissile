1af196b94352b658fe9cbb6abc253acbc88b7692
Paul Sinnett <paul.sinnett@gmail.com>
2020-10-06 14:14:17 +0100

    experiment with homing behaviour

---
+++ Assets/Homing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
--- journal.md
+++ journal.md
@@ -3,3 +3,5 @@
 ## 06/10/2020
 
 I started by creating an empty project. But I should have started with the log file so I could record my start time.
+
+I created a variable for speed of type float, but I needed to put an `f` on the end.
